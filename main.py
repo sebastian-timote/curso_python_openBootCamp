2ce2740915e213219b59a17098a5b77b7cf2785b
sebastian-timote <stimotev11@gmail.com>
2023-01-21 00:13:36 -0500

    ejemplo sorted y switch

--- main.py
+++ main.py
@@ -82,7 +82,7 @@
 #for numero in range(len(lista)):
 #    print(lista[numero])
 
-lista = ['hola','mensaje','adios']
+#lista = ['hola','mensaje','adios']
 #for palabra in lista:
 #    print('palabra actual: ',palabra)
 #    if palabra == 'mensaje':
@@ -96,6 +96,48 @@ lista = ['hola','mensaje','adios']
 
 #forma con NOT IN
 
-if 'mensaje' not in lista:
-    print('éncontre palabra mensaje')
+#if 'mensaje' not in lista:
+#    print('éncontre palabra mensaje')
+
+#lista = [3,4,1,2,5]
+#print(lista)
+#listaOrdenada = sorted(lista)#ordena la lista consecutivamente
+#listaOrdenada = sorted(lista, reverse=True)#la ordena pero en reversa
+#print(listaOrdenada)
+#lista = ['A','a','p','P','Z','z']
+#print(lista)
+#listaOrdenada = sorted(lista)
+#print(listaOrdenada)#lo ordena en el orden del formato ASCCI
+
+#valor = 3
+
+#if valor ==1:
+#    print('valor es 1')
+#elif valor ==2:
+#    print('valor es 2')
+#elif valor ==3:
+#    print('valor es 3')
+#elif valor ==4: 
+#    print('valor es 4')
+#elif valor ==5:
+#    print('valor es 5')
+
+#forma de hacer un switch apartir de la version python 3.10
+#no funciono el switch en python 3.10 :'(
+#valor = 4
+
+#match 4:
+#    case 1:
+#        print('valor es 1')
+#    case 2:
+#        print('valor es 2')
+#    case 3:
+#        print('valor es 3')
+#    case 4:
+#        print('valor es 4')
+#    case 5:
+#        print('valor es 5')
+
+        
+
 
