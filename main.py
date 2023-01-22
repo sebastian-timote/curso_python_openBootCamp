10faac8203b98ffc4406746e61cd9596f2b1a48f
sebastian-timote <stimotev11@gmail.com>
2023-01-21 23:03:56 -0500

    ejemplo funciones sin parametro

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
-lista = ['hola', 'mensaje', 'adios']
-#pass -> se usa cuando quieres que el programa continue sin errores aunque todavia no tenga programacion
- 
-for palabra in lista:
-    pass
-if 5 >= 6:
-    pass
-while False:
-    pass
+def miFuncion():
+    print('en mi funcion')
+    for i in range(1,5):
+        print(i)
+        
+
+print('before')
+miFuncion()
+print('after')
+
+
