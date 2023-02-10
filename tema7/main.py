8494ad8b076bfe397ff5793312b6d1fc01e119a5
sebastian-timote <stimotev11@gmail.com>
2023-02-10 15:40:44 -0500

    ejemplo creando paquetes y accediendolos

--- tema7/main.py
+++ tema7/main.py
@@ -1,14 +1,8 @@
 
-#sys.path es una variable que almacena rutas de modulos
-import sys
-#en esta linea agregamos una nueva ruta ala variable path para que
-# busque alli modulos que vamos a crear en este caso el modulo saluda.py
-sys.path.append('/home/unfulanoubuntu/archivos_test/python/mismodulos')
-import pprint
-pprint.pprint(sys.path)
-import saluda
+import operaciones2.suma
 def main():
-    saluda.saluda('sebas')
-
+    #print(operaciones2.suma.suma(2,2))
+    mp = operaciones2.suma.Multiplicador()
+    print(mp.multiplica(5,5))
 if __name__ == '__main__':
     main()
