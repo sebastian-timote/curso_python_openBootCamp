f3dea1151cdcdb52dac470fbdf6db63696d0ab6a
sebastian-timote <stimotev11@gmail.com>
2023-02-14 23:10:37 -0500

    formateo de cadenas forma 1, 2 y 3

---
+++ tema8/main.py
@@ -0,0 +1,23 @@
+#formateo de cadenas
+numero = 511
+texto = "quijote"
+otromas = 1.2
+#TODO forma 1 antes de python 2.6 se hacia de esta manera
+
+#print("el numero es %d y el texto es %s y tiene %.2f" % (numero,texto,otromas))
+
+#TODO forma 2 despues de python 2.6 se empezo a utilizar esta forma
+# y se dejo de utilizar hasta la version 3.6
+
+#print("el numero es {} y el texto es {} y tiene {}".format(numero,texto,otromas))
+#diferentes formas indicando el orden como si fuera un array
+#print("el numero es {1} y el texto es {0} y tiene {2}".format(texto,numero,otromas))
+# o puede ser tambien con parametros nombrados
+#print("el numero es {n1} y el texto es {t1} y tiene {f1}".format(n1=numero,t1=texto,f1=otromas))
+
+#TODO forma 3 despues de python 3.6 se empezo a utilizar esta forma las fstrings cadenas tipo f
+# no es muy utiliza por desconocimiento
+# ademas que se puede invocar metodos de python
+def suma(a,b):
+    return a + b 
+print(f'el numero es {suma(numero,numero)} y el texto es {texto.upper()} y tiene {otromas}')
