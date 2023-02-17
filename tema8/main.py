582232b407a0c94ab30c1059896f4ddc76579749
sebastian-timote <stimotev11@gmail.com>
2023-02-17 14:25:02 -0500

    ejemplo escritura de datos en fichero

--- tema8/main.py
+++ tema8/main.py
@@ -1,56 +1,29 @@
-# TODO conjunto de funciones de manipulacion de ficheros
-# r: lectura
-# a: append
-# w: escritura
-# x: create
-
-#t: texto plano
-#b: archivo binario
-
-#+:
-#f = open('/../../../etc/passwd', 'r')
-#datos = f.read()# si adentro se le coloca un valor numerico seria la cantidad de caracteres que queremos leer
-#datos = f.readline()# lee la primera linea
-#datos = f.readline()# lee la segunda linea
-#f.close()
-#print(datos)
-#TODO RECORRER UN ARCHIVO Y LEER LO LINEA POR LINEA
-#datos = None
-#while(datos != ""):
-#    datos = f.readline()
-#    print(datos)
+# escritura de ficheros
+# a -> agrega nuevo texto al final
+# w -> escribe en el archivo especificado borrando lo antiguo que contenga
+#f = open('mifichero.txt','w')
+#f.write('datos\n')
+#f.write('datos2\n')
 #f.close()
 
-#TODO LEE LOS DATOS Y LOS PONE EN UNA LISTA
-
-#datos = f.readlines()
-#print(datos[1])
-#f.close()
-
-#TODO OTRO EJEMPLO DE LECTURA DE FICHEROS
-#busca en una archivo del sistema lo recorre y agrega en una lista los user
-# y lo simprime en la terminal
-def main():
-    usuarios = listarUsuario()
-    print(usuarios)
-    for user in usuarios:
-        print(f'Usuario: {user}')
-def listarUsuario():
-    f = open('/../../../etc/passwd', 'r')
-    datos = f.readlines()
+#TODO escribir lineas
+def escribe(archivo,data):
+    f = open(archivo,'w')
+    for lineas in data:
+        if not lineas.endswith('\n'):
+            lineas = lineas + '\n'
+        f. write(lineas)
     f.close()
-    resultado = []
-    for linea in datos:
-        if ((linea.startswith('sys') == True) or (linea[0] == '_')):
-            continue
-        partes = linea.split(':')
-        resultado.append(partes[0])
-    return resultado
+
+lista = [
+    'una linea',
+    'dos lineas',
+    'tres lineas'
+]
+escribe('mifichero.txt', lista)
 
 
-    
-if __name__ == '__main__':
-    main()
+
 
 
 
