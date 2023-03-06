9b9649355b782eda6be7a7c1b6ba9a14e780074e
sebastian-timote <stimotev11@gmail.com>
2023-03-06 16:30:44 -0500

    ejemplo geometria pack()

---
+++ tema10/main.py
@@ -0,0 +1,45 @@
+# el run de vs code no funciono toco ejecutarlo directo de consola de linux
+#pack -> se utiliza cuando se quiere situar elementos de arriba abajo
+# o de izq a der
+#bg ->background
+#fg ->foreground
+#fill-> llenar un espacio en el eje x, y o both
+#expand-> expande y lo deja en el centro
+#side -> lo posiciona a la izq o derecha
+import tkinter
+window = tkinter.Tk()
+
+#label_saludo = tkinter.Label(window, text="hola!", bg="yellow", fg="blue")# creo etiqueta
+#label_saludo.pack(ipadx=50,ipady=50, fill='x')#posiciono mi etiqueta dentro de la ventana
+#label_saludo.pack(ipadx=50,ipady=50, side='left')#posiciono mi etiqueta dentro de la ventana
+
+#label_adios = tkinter.Label(window, text="Adios", bg="red",fg="white")
+#label_adios.pack(fill='both',expand=True)
+#label_adios.pack(fill='both',side='right')
+
+#TODO EJEMPLO DOS
+
+#label1 = tkinter.Label(window, text="label1", bg="yellow",fg="blue")
+#label1.pack(ipadx=45,ipady=15, fill='x')
+
+#label2 = tkinter.Label(window, text="label2", bg="purple",fg="white")
+#label2.pack(ipadx=45,ipady=15, fill='x')
+
+#label3 = tkinter.Label(window, text="label3", bg="blue",fg="white")
+#label3.pack(ipadx=45,ipady=15, fill='x')
+
+#label4 = tkinter.Label(window, text="label4", bg="red",fg="white")
+#label4.pack(ipadx=15,ipady=15, side="left")
+
+#label5 = tkinter.Label(window, text="label5", bg="yellow",fg="black")
+#label5.pack(ipadx=15,ipady=15, side="left")
+
+#label6 = tkinter.Label(window, text="label6", bg="green",fg="white")
+#label6.pack(ipadx=15,ipady=15, side="right")
+
+#TODO GEOMETRIA GRIP
+
+
+
+window.mainloop()#para que se pueda ver la ventana se tiene que poner un main loop
+
