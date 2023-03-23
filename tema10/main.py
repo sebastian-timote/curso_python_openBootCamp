8df99b4e7ebca2046de562ab421a8fd4fb3b9781
sebastian-timote <stimotev11@gmail.com>
2023-03-23 16:27:35 -0500

    geometria pack, grip, place

--- tema10/main.py
+++ tema10/main.py
@@ -37,9 +37,60 @@ window = tkinter.Tk()
 #label6 = tkinter.Label(window, text="label6", bg="green",fg="white")
 #label6.pack(ipadx=15,ipady=15, side="right")
 
-#TODO GEOMETRIA GRIP
+##TODO GEOMETRIA GRIP
+##maneja posicionamiento como una hoja de excel
+##(0,0) (1,0) (2,0)
+##(0,1) (1,1) (2,1)
+##(0,2) (1,2) (2,2)
+##(0,3) (1,3) (2,3)
+##sticky-> para que la label quede fija
+## padx -> es como un padding en css
+## ipadx -> es como un margin en css
+##TODO EJEMPLO GRIP
+from tkinter import ttk
+#window.columnconfigure(0,weight=3)
+#window.rowconfigure(1, weight=3)
+##usuario
+##etiqueta usuario
+#username_label = ttk.Label(window, text="username:")
+#username_label.grid(column=0,row=0, sticky=tkinter.W, padx=5, pady=5)
+##campo usuario
+#username_entry = ttk.Entry(window)
+#username_entry.grid(column=1, row=0, sticky=tkinter.E, padx=5, pady=5)
+##password
+##etiqueta contraseña
+#password_label = ttk.Label(window, text="password:")
+#password_label.grid(column=0,row=1, sticky=tkinter.W, padx=5, pady=5)
+##campo contraseña
+#password_entry = ttk.Entry(window, show="*")
+#password_entry.grid(column=1, row=1, sticky=tkinter.E, padx=5, pady=5)
+##boton login
+#login_button = ttk.Button(window, text="login")
+#login_button.grid(column=2, row=2, sticky=tkinter.E, padx=5, pady=5) 
+##TODO FIN EJEMPLO GRIP
 
+##TODO GEOMETRIA PLACE
+## posicionamiento absoluto -> no se mueve asi cambie de resolucion el tamaño de la aplicacion
+## posicionamiento relativo -> cambia deacuerdo el tamano de la aplicacion
 
+#label1 = ttk.Label(window, text="posicionamiento absoluto", background="blue", foreground="white")
+#label1.place(x=10,y=50)
+
+#label2 = ttk.Label(window, text="posicion relativa", background="red", foreground="yellow")
+#label2.place(relx=0.10,rely=0.15, relwidth=0.5, anchor="ne")
+
+##TODO EJEMPLO PLACE
+import random
+
+colors = ['blue','red','yellow','purple','black']
+
+for x in range(0,10):
+    color = random.randint(0, len(colors)-1 )
+    color2 = random.randint(0, len(colors)-1 )
+    label = ttk.Label(window, text="etiqueta!!", background=colors[color], foreground=colors[color2])
+    label.place(x=random.randint(0,100),y=random.randint(0,100))
+
+##TODO FIN EJEMPLO PLACE
 
 window.mainloop()#para que se pueda ver la ventana se tiene que poner un main loop
 
