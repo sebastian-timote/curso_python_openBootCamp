4413c91a0a4032a8377beecb4e009cca1903922b
sebastian-timote <stimotev11@gmail.com>
2023-03-23 23:09:55 -0500

    ejemplos frame, radiobutton,listbox

--- tema10/main.py
+++ tema10/main.py
@@ -1,96 +1,49 @@
-# el run de vs code no funciono toco ejecutarlo directo de consola de linux
-#pack -> se utiliza cuando se quiere situar elementos de arriba abajo
-# o de izq a der
-#bg ->background
-#fg ->foreground
-#fill-> llenar un espacio en el eje x, y o both
-#expand-> expande y lo deja en el centro
-#side -> lo posiciona a la izq o derecha
+
+import sys
 import tkinter
 window = tkinter.Tk()
-
-#label_saludo = tkinter.Label(window, text="hola!", bg="yellow", fg="blue")# creo etiqueta
-#label_saludo.pack(ipadx=50,ipady=50, fill='x')#posiciono mi etiqueta dentro de la ventana
-#label_saludo.pack(ipadx=50,ipady=50, side='left')#posiciono mi etiqueta dentro de la ventana
-
-#label_adios = tkinter.Label(window, text="Adios", bg="red",fg="white")
-#label_adios.pack(fill='both',expand=True)
-#label_adios.pack(fill='both',side='right')
-
-#TODO EJEMPLO DOS
-
-#label1 = tkinter.Label(window, text="label1", bg="yellow",fg="blue")
-#label1.pack(ipadx=45,ipady=15, fill='x')
-
-#label2 = tkinter.Label(window, text="label2", bg="purple",fg="white")
-#label2.pack(ipadx=45,ipady=15, fill='x')
-
-#label3 = tkinter.Label(window, text="label3", bg="blue",fg="white")
-#label3.pack(ipadx=45,ipady=15, fill='x')
-
-#label4 = tkinter.Label(window, text="label4", bg="red",fg="white")
-#label4.pack(ipadx=15,ipady=15, side="left")
-
-#label5 = tkinter.Label(window, text="label5", bg="yellow",fg="black")
-#label5.pack(ipadx=15,ipady=15, side="left")
-
-#label6 = tkinter.Label(window, text="label6", bg="green",fg="white")
-#label6.pack(ipadx=15,ipady=15, side="right")
-
-##TODO GEOMETRIA GRIP
-##maneja posicionamiento como una hoja de excel
-##(0,0) (1,0) (2,0)
-##(0,1) (1,1) (2,1)
-##(0,2) (1,2) (2,2)
-##(0,3) (1,3) (2,3)
-##sticky-> para que la label quede fija
-## padx -> es como un padding en css
-## ipadx -> es como un margin en css
-##TODO EJEMPLO GRIP
 from tkinter import ttk
-#window.columnconfigure(0,weight=3)
-#window.rowconfigure(1, weight=3)
-##usuario
-##etiqueta usuario
-#username_label = ttk.Label(window, text="username:")
-#username_label.grid(column=0,row=0, sticky=tkinter.W, padx=5, pady=5)
-##campo usuario
-#username_entry = ttk.Entry(window)
-#username_entry.grid(column=1, row=0, sticky=tkinter.E, padx=5, pady=5)
-##password
-##etiqueta contraseña
-#password_label = ttk.Label(window, text="password:")
-#password_label.grid(column=0,row=1, sticky=tkinter.W, padx=5, pady=5)
-##campo contraseña
-#password_entry = ttk.Entry(window, show="*")
-#password_entry.grid(column=1, row=1, sticky=tkinter.E, padx=5, pady=5)
-##boton login
-#login_button = ttk.Button(window, text="login")
-#login_button.grid(column=2, row=2, sticky=tkinter.E, padx=5, pady=5) 
-##TODO FIN EJEMPLO GRIP
+##TODO FRAME --> AGRUPACION DE COSAS, PARECE UN DIV
+##crear el frame
+#frame = ttk.Frame(window)
+#frame2 = ttk.Frame(window)
+## crear etiqueta
+#label = ttk.Label(frame, text="usando frame")
+#label2 = ttk.Label(frame2, text="frame dos")
+##ubicar la etiqueta
+#label.grid(column=0,row=0, sticky=tkinter.W, padx=50,pady=50)
+#label2.grid(column=0,row=1,sticky=tkinter.E, padx=50, pady=50)
 
-##TODO GEOMETRIA PLACE
-## posicionamiento absoluto -> no se mueve asi cambie de resolucion el tamaño de la aplicacion
-## posicionamiento relativo -> cambia deacuerdo el tamano de la aplicacion
+##ubicar los frame
+#frame.grid(column=0,row=0, sticky=tkinter.W)
+#frame2.grid(column=0,row=1, sticky=tkinter.W)
 
-#label1 = ttk.Label(window, text="posicionamiento absoluto", background="blue", foreground="white")
-#label1.place(x=10,y=50)
+##TODO LIST BOX --> LISTA DE SELECCION
 
-#label2 = ttk.Label(window, text="posicion relativa", background="red", foreground="yellow")
-#label2.place(relx=0.10,rely=0.15, relwidth=0.5, anchor="ne")
+#lista = ['windows','macOS','linux', 'msDOS','amigaOS', 'beOS', 'OS/2']
+#lista_items = tkinter.StringVar(value=lista) #para convertir la lista a un formato que entienda ttk o tcl
+#listBox = tkinter.Listbox(window, height=60, listvariable=lista_items)
+#listBox.grid(column=0,row=0, sticky=tkinter.W)
 
-##TODO EJEMPLO PLACE
-import random
+##TODO RADIOBUTTON --> 
+#def miFuncion():
+#    print('clicado')
+#seleccionado = tkinter.StringVar()# agrupa un radio button de otro
+##sitodos tiene esta misma variable solo se podra escoger uno de ellos 
+#r1 = ttk.Radiobutton(window, text="Si", value='1', variable=seleccionado, command=miFuncion)
+#r2 = ttk.Radiobutton(window, text="No", value='2', variable=seleccionado)
+#r3 = ttk.Radiobutton(window, text="Tal vez", value='3', variable=seleccionado)
 
-colors = ['blue','red','yellow','purple','black']
+#r1.grid(column=0,row=1, padx=5,pady=5)
+#r2.grid(column=0,row=2, padx=5,pady=5)
+#r3.grid(column=0,row=3, padx=5,pady=5)
 
-for x in range(0,10):
-    color = random.randint(0, len(colors)-1 )
-    color2 = random.randint(0, len(colors)-1 )
-    label = ttk.Label(window, text="etiqueta!!", background=colors[color], foreground=colors[color2])
-    label.place(x=random.randint(0,100),y=random.randint(0,100))
+#seleccionado2 = tkinter.StringVar()
+#rs1 = ttk.Radiobutton(window, text="Si", value='12', variable=seleccionado2)
+#rs1.grid(column=1,row=0, padx=5,pady=5)
 
-##TODO FIN EJEMPLO PLACE
+#TODO EVENTOS ->son reacciones a una accion exterior como el teclado, mouse, etc
+
 
 window.mainloop()#para que se pueda ver la ventana se tiene que poner un main loop
-
+sys.exit(0)
