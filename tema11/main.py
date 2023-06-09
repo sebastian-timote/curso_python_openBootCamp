328266ceb8f36357962961e36192270026073bcb
sebastian-timote <stimotev11@gmail.com>
2023-06-08 22:35:03 -0500

    insertando y seleccionan db

--- tema11/main.py
+++ tema11/main.py
@@ -1,9 +1,37 @@
 import sqlite3
+import getpass
 
-conn = sqlite3.connect('miaplicacion.db')
-cursor = conn.cursor()
-rows = cursor.execute('SELECT * FROM users WHERE username="vroman";')
-for row in rows:
-    print(row)
-cursor.close()
-conn.close()
+def main():
+    crear_usuario(4, "vicky", "otraclave")
+def main2():
+    username = input("Nombre de ususario: ")
+    password = getpass.getpass("Contraseña: ")
+    if (verifica_credenciales(username, password)):
+        print("login correcto")
+    else:
+        print("login incorrecto")
+
+def verifica_credenciales(username,password):
+    conn = sqlite3.connect('miaplicacion.db')
+    cursor = conn.cursor()
+    query = f"SELECT id FROM users WHERE username='{username}' AND password='{password}'"
+    print("query a ejecutar: ", query)
+    rows= cursor.execute(query)
+    data = rows.fetchone()
+    print("data es:", type(data))
+    cursor.close()
+    conn.close()
+
+def crear_usuario(identificador,usuario,clave):
+    conn = sqlite3.connect('miaplicacion.db')
+    cursor = conn.cursor()
+    query = f"INSERT INTO users(id,username,password)  VALUES(?,?,?)"
+    rows= cursor.execute(query, (identificador,usuario,clave))
+    print(type(rows))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    
+if __name__ == '__main__':
+    main()  
+
